14d1a2b627c200f0a14fe8158378206878f2daa1
Francesco Pochetti <fra.pochetti@gmail.com>
2015-10-13 13:52:02 +0200

    removing personal key to F2F API

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,6 @@
 class Recipe
     include HTTParty
-    ENV["FOOD2FORK_KEY"] = '244f763c83d55fa58b25d86a9033d765'
+    ENV["FOOD2FORK_KEY"] = 'your API KEY'
   	base_uri 'http://food2fork.com/api'
   	default_params key: ENV["FOOD2FORK_KEY"]
   	format :json
