a5abda51eb10ecd1defcd2061b9abd6f9692831b
Ammar A. Raja <ammarforuck@gmail.com>
2016-02-01 16:23:48 +0500

    'hopefully'

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,6 @@
 class Recipe
     include HTTParty
-    ENV["FOOD2FORK_KEY"] = 'your API KEY'
+    ENV["FOOD2FORK_KEY"] = '839dd836ffcd3ad4df23afb769577b5e'
   	base_uri 'http://food2fork.com/api'
   	default_params key: ENV["FOOD2FORK_KEY"]
   	format :json
